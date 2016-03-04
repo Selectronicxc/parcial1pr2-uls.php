8e0494c9a3d2fd244f6afd4ce30214b4295ae496
Carlos_Mejia <root@CarlosMejia-PC>
2016-03-03 19:50:10 -0600

    Subiendo archivo de CarlosMejia

---
+++ parcial1pr2-uls.php
@@ -0,0 +1,9 @@
+<?php
+//Programa realizado por Carlos Alberto Mejia Barahona
+//MB01132820
+$nota1 = 8.7;
+$nota2 = 7.8;
+$nota3 = 9.0;
+$promedio = ($nota1+$nota2+$nota3)/3;
+echo "Su promedio es: ".$promedio
+?>
